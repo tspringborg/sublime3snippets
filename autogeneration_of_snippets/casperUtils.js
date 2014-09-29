40de4a71c3b1e531f9b882c1e451486df11af0a4
Thomas Springborg <ts@molamil.com>
2014-09-29 12:22:31 +0200

    Added underscore snippets. Added casperjs script to auto generate underscore snippets from the documentation

---
+++ autogeneration_of_snippets/casperUtils.js
@@ -0,0 +1,24 @@
+// my module, stored in universe.js
+// patching phantomjs' require()
+var require = patchRequire(require);
+
+exports.setupEvents = function(casperInstance) {
+    var casper = casperInstance;
+
+	casper.on('http.status.404', function(resource) {
+	    this.echo('wait, this url is 404: ' + resource.url);
+	});
+
+	casper.on('http.status.500', function(resource) {
+	    this.echo('woops, 500 error: ' + resource.url);
+	});
+
+	casper.start('http://mywebsite/404', function() {
+	    this.echo('We suppose this url return an HTTP 404');
+	});
+};
+exports.stepLog = function(casperInstance){
+	require('utils').dump(casperInstance.steps.map(function(step) {
+	    return step.toString();
+	}));
+}
