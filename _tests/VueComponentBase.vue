a7a89a790f23e811a85f064e25deb159796103db
Thomas Springborg <ts@Thomass-MacBook-Pro-2.local>
2017-01-20 14:48:52 +0100

    improved vue template

---
+++ _tests/VueComponentBase.vue
@@ -0,0 +1,19 @@
+<template>
+    <div></div>
+</template>
+
+<script>
+    export default {
+        name: 'VueComponentBase',
+        data() {
+            return {
+            }
+        },
+        methods: {
+        },
+    }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="sass">
+</style>
---
+++ _tests/VueComponentFull.vue
@@ -0,0 +1,68 @@
+<template>
+    <div></div>
+</template>
+
+<script>
+    export default {
+        name: 'VueComponentFull',
+        data() {
+            return {
+            }
+        },
+        components: {
+
+        },
+        props: {
+
+        },
+        directives: {
+
+        },
+        filters: {
+
+        },
+        methods: {
+
+        },
+        computed: {
+
+        },
+        watch: {
+
+        },
+        beforeCreate() {
+
+        },
+        beforeMount() {
+
+        },
+        mounted() {
+
+        },
+        created() {
+
+        },
+        beforeUpdate() {
+
+        },
+        updated() {
+
+        },
+        activated() {
+
+        },
+        beforeDestroy() {
+
+        },
+        destroyed() {
+
+        },
+        deactivated() {
+
+        },
+    }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="sass">
+</style>
